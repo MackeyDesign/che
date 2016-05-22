8d782933208801fd9b74aaba03f6be3ad190fbb9
rnavagamuwa <randikanavagamuwa@gmail.com>
2016-05-23 00:06:59 +0530

    my extension updated

--- plugins/plugin-orion/che-plugin-orion-editor/src/main/java/org/eclipse/che/ide/editor/orion/client/examples/myTest.java
+++ plugins/plugin-orion/che-plugin-orion-editor/src/main/java/org/eclipse/che/ide/editor/orion/client/examples/myTest.java
@@ -14,33 +14,61 @@ package org.eclipse.che.ide.editor.orion.client.examples;
 
  */
 
+import com.google.gwt.editor.client.Editor;
+import com.google.inject.assistedinject.Assisted;
+import com.google.inject.assistedinject.AssistedInject;
 import org.eclipse.che.ide.api.action.Action;
 import org.eclipse.che.ide.api.action.ActionEvent;
 
 import com.google.inject.Inject;
+import org.eclipse.che.ide.api.editor.EditorAgent;
+import org.eclipse.che.ide.api.editor.EditorPartPresenter;
+import org.eclipse.che.ide.api.editor.EditorProvider;
+import org.eclipse.che.ide.api.editor.EditorRegistry;
+import org.eclipse.che.ide.api.editor.annotation.AnnotationModel;
+import org.eclipse.che.ide.api.editor.annotation.AnnotationModelEvent;
+import org.eclipse.che.ide.api.editor.texteditor.TextEditorPresenter;
 import org.eclipse.che.ide.api.notification.NotificationManager;
+import org.eclipse.che.ide.editor.orion.client.OrionEditorModule;
+import org.eclipse.che.ide.editor.orion.client.OrionEditorWidget;
+import org.eclipse.che.ide.editor.orion.client.OrionTextEditorFactory;
+import org.eclipse.che.ide.editor.orion.client.jso.OrionAnnotationModelOverlay;
 import org.eclipse.che.ide.editor.orion.client.jso.OrionAnnotationOverlay;
-import org.eclipse.che.ide.editor.orion.client.jso.OrionEditorOverlay;
 import org.eclipse.che.ide.editor.orion.client.jso.OrionStyleOverlay;
+
+import javax.validation.constraints.NotNull;
 
 
 public class myTest extends Action {
     private NotificationManager notificationManager;
+    private EditorAgent editorAgent;
+    private AnnotationModelEvent annotationModelEvent;
+    private OrionTextEditorFactory orionTextEditorFactory;
+    private EditorRegistry editorRegistry;
+    private EditorProvider editorProvider;
+    private EditorPartPresenter editorPartPresenter;
+    private OrionEditorModule orionEditorModule;
+
     @Inject
-    public myTest(MyResources resources, NotificationManager notificationManager) {
+    public myTest(MyResources resources, NotificationManager notificationManager,EditorAgent editorAgent,OrionEditorModule orionEditorModule) {
         super("My Action", "My Action Description", null, resources.MyProjectTypeIcon());
         this.notificationManager = notificationManager;
+        this.editorAgent = editorAgent;
+        this.orionEditorModule = orionEditorModule;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
-        notificationManager.notify("Hello World test now !");
+        this.editorPartPresenter = editorAgent.getActiveEditor();
+        String temp = editorPartPresenter.getEditorInput().getToolTipText();
+        notificationManager.notify( temp);
+       // editorAgent.getActiveEditor().close(true);
+       // TextEditorPresenter<OrionEditorWidget> editor = (TextEditorPresenter<OrionEditorWidget>) editorAgent.getActiveEditor();
+         //editorProvider.getEditor();
         final OrionAnnotationOverlay annotation = OrionAnnotationOverlay.create();
 
         OrionStyleOverlay styleOverlay = OrionStyleOverlay.create();
         //styleOverlay.setStyleClass(className);
-
         annotation.setStart(150);
         annotation.setEnd(150);
         annotation.setRangeStyle(styleOverlay);
